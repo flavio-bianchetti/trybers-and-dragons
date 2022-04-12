3a0a561771aaf3dd1a4f196933880fccb28ab736
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-12 19:02:20 -0300

    feat: Character class changed

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -67,7 +67,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  public attack(value: Fighter): void {
+  public attack(value: Fighter | SimpleFighter): void {
     value.receiveDamage(this._strength);
   }
 
